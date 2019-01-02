bbedbdd517d33fc426d187858b4c1edd2e8375ef
Gal Weizman <weizmangal@gmail.com>
2019-01-02 23:38:41 +0200

    upload code

---
+++ index.js
@@ -0,0 +1,26 @@
+// https://stackoverflow.com/questions/27272029/how-to-create-a-regexp-to-find-all-strings-in-js-files-or-json
+const REG_EXP = /(['"])(?:\\\1|(?!\1).)*?\1/g;
+
+function find (str, cb, withQuotes = false) {
+  let res = REG_EXP.exec (str);
+
+  while (res) {
+    cb (withQuotes ? res[0] : res[0].slice (1, res[0].length - 1));
+    res = REG_EXP.exec (str);
+  }
+}
+
+function replace (str, cb, withQuotes = false) {
+  let output = str;
+
+  function _replace (res) {
+    output = output.replace (res, cb (res));
+  }
+
+  find (str, _replace, withQuotes);
+
+  return output;
+}
+
+module.exports.find = find;
+module.exports.replace = replace;
